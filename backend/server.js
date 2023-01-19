174892acdfce8023cc6b1eb87db112ec18234764
Daktic <g@eye-catcher.com>
2023-01-19 16:58:59 -0500

    added comments

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const userRoute = require("./routes/user");
-
+const photoRoute = require("./routes/photos");
 const app = express()
 
 
 app.use('/user', userRoute);
-
+app.use('/photo', photoRoute);
