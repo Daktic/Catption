5572e933b708a741c56713c21818cf09b1604abe
Daktic <g@eye-catcher.com>
2023-01-20 17:37:47 -0500

    did I make changes here?

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const userRoute = require("./routes/user");
 const photoRoute = require("./routes/photos");
 const app = express()
 
-
+app.use(bodyParser.json())
 app.use('/user', userRoute);
 app.use('/photo', photoRoute);
+
+
+
+const port = 80;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
