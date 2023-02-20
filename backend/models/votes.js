3c8d20b87172f2c67246d9c0e4e5c9394d32960f
Daktic <g@eye-catcher.com>
2023-02-20 12:49:44 -0500

    added a votes model

---
+++ backend/models/votes.js
@@ -0,0 +1,41 @@
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Votes extends Model {}
+
+  Votes.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      commentId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Photos",
+          key: "id",
+        },
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      value: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        validate: {
+          min: -1, // minimum value
+          max: 1, // maximum value
+        },
+      },
+    },
+    { sequelize }
+  );
+  return Votes;
+};
